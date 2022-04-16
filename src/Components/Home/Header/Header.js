1b9c3b7f657ff9046953281d297f8675f604f0b0
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-04-16 23:40:17 +0600

    navbar added

---
+++ src/Components/Home/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Jahid Photography</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Blogs</Nav.Link>
+                            <Nav.Link href="#deets">About</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Login
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
